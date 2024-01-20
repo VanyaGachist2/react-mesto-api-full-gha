8167d98faa66ade8ecabfc7362c430f272ced69e
Ivan <vany4golos@yandex.ru>
2024-01-20 16:01:25 +0300

    second try

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-require('dotenv').config;
 const { PORT = 3000 } = process.env;
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,25 +8,14 @@ const { login, createUser } = require('./controllers/users.js');
 const auth = require('./middlewares/auth.js');
 const { validationCreateUser, validationLogin } = require('./middlewares/validation.js');
 const NotFoundError = require('./errors/NotFoundError.js'); // 404
-const { requestLogger, errorLogger } = require('./middlewares/logger.js');
-const cors = require('./middlewares/cors.js');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
 });
 
 app.post('/signup', validationCreateUser, createUser);
@@ -41,8 +29,6 @@ app.use('/', cardRoutes);
 app.use(() => {
   throw new NotFoundError('Такой страницы нет');
 })
-
-app.use(errorLogger);
 
 app.use(errors());
 
